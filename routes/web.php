20847b5d838db5a102e2f28c581e923e6ca4d78f
zckongbai <zckongbai@163.com>
2017-11-01 16:25:33 +0800

    google recaptcha

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,8 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('contact', function() {
+    return View::make('contact');
+});
+Route::post('contact', 'EnquiryController@index');
